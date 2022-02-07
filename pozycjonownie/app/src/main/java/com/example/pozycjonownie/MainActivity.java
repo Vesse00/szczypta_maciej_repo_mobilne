8e2f8ef44ccc96baf83c0f1fa803b87537256e7a
Vesse00 <55257923+Vesse00@users.noreply.github.com>
2022-02-07 14:54:32 +0100

    progr

--- pozycjonownie/app/src/main/java/com/example/pozycjonownie/MainActivity.java
+++ pozycjonownie/app/src/main/java/com/example/pozycjonownie/MainActivity.java
@@ -2,7 +2,17 @@ package com.example.pozycjonownie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button save_btn = findViewById(R.id.save_btn);
+        Button load_btn = findViewById(R.id.load_btn);
+        EditText text_window = findViewById(R.id.editTextTextPersonName);
+
+        String filename = "data.txt";
+        Context context = this;
+
+        File file = new File(context.getFilesDir(), filename);
+
+        save_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = text_window.getText().toString();
+
+                try {
+                    System.out.println(context.getFilesDir());
+                    FileWriter fw = new FileWriter(file);
+                    fw.write(text);
+                    fw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        load_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Scanner sc = new Scanner(file);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 }
